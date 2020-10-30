ce70961218c55356ea1f0fe6cd7b208370f22cd5
Julio Brazao <jotabrazao@gmail.com>
2020-10-29 20:57:05 -0400

    Adjusting port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const app = require('./config/server')
 const database = require('./config/database')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 80
 
 /* CREATE */
 app.route('/')
